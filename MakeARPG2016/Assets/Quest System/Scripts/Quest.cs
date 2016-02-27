b622fc4a60467dd87c75c7775771bbcd6b3f899c
HardlyBriefDan <dadamson90@gmail.com>
2016-02-27 11:48:41 -0500

    Episode 1 Commit
    Adds basic quest structure code

---
+++ MakeARPG2016/Assets/Quest System/Scripts/Quest.cs
@@ -0,0 +1,33 @@
+﻿//using a namespace to ogranize all of our quest information
+namespace QuestSystem
+{
+    //basic quest class object
+    public class Quest
+    {
+
+        //Name
+        //DescriptionSummary
+        //Quest Hint
+        //Quest Dialog
+        //sourceID
+        //questID
+        //chain quest and the next quest is blank
+        //chainquestiD
+        private IQuestInformation information;
+        public IQuestInformation Information
+        {
+            get { return information; }
+        }
+
+
+        //objectives
+        //bonus objectives
+        //rewards
+        //events
+            //on completetion
+            //on failed
+            //on update
+
+    }
+}
+
---
+++ MakeARPG2016/Assets/Quest System/Scripts/QuestObjective.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using QuestSystem;
+
+public class QuestObjective : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Quest q = new Quest();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
